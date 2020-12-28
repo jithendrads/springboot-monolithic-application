73fe6141a69365693cc774e0cc4568944d8d79f1
Jithendra <jithendrads@gmail.com>
2020-12-28 10:54:58 +0530

    Customer Repository class

--- telecom-application/src/main/java/com/jithendra/telecomapplication/repository/CustomerRepository.java
+++ telecom-application/src/main/java/com/jithendra/telecomapplication/repository/CustomerRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.jithendra.telecomapplication.entity.Customer;
 
-
+  
 public interface CustomerRepository extends JpaRepository<Customer, Long>{
 
 }
