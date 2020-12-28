e0d79d56477cb0525820e5e3f97730b4a4672030
Jithendra <jithendrads@gmail.com>
2020-12-28 11:17:50 +0530

    Starting point of the execution

--- telecom-application/src/main/java/com/jithendra/telecomapplication/TelecomApplication.java
+++ telecom-application/src/main/java/com/jithendra/telecomapplication/TelecomApplication.java
@@ -2,7 +2,7 @@ package com.jithendra.telecomapplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//starting point of the application execution
+//starting point of the execution
 @SpringBootApplication
 public class TelecomApplication {
 
@@ -10,4 +10,4 @@ public class TelecomApplication {
 		SpringApplication.run(TelecomApplication.class, args);
 	}
    
-}
+} 
