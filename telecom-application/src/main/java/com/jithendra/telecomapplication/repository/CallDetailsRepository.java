01285764fd568d265bcf108d8a0d1c68ae15fa50
Jithendra <jithendrads@gmail.com>
2020-12-28 10:54:33 +0530

    Calls Details repository class

--- telecom-application/src/main/java/com/jithendra/telecomapplication/repository/CallDetailsRepository.java
+++ telecom-application/src/main/java/com/jithendra/telecomapplication/repository/CallDetailsRepository.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Repository;
 
 import com.jithendra.telecomapplication.entity.CallDetails;
 
-
+  
 public interface CallDetailsRepository extends JpaRepository<CallDetails, Long> {
 	List<CallDetails> findByCalledBy(long calledBy);
 	
